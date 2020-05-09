67a89b1461c9d725adbf85504742194ef1b25662
Hendrik <hendrik.hilsberg@epfl.ch>
2020-05-09 18:42:44 +0200

    commentaire dans TOF

--- TOF_processing.c
+++ TOF_processing.c
@@ -1,10 +1,3 @@
-/*
- * TOF_processing.c
- *
- *  Created on: 29 avr. 2020
- *      Author: Hendrik
- *
- */
 #include "ch.h"
 #include "hal.h"
 #include <main.h>
@@ -23,8 +16,8 @@
 #define SCAN_ANGLE		PI/9	//angle in rad
 #define ANGLE_90DEG		PI/2
 #define TIME_LEFT		240		//time to go SCAN_ANGLE to the left
-#define TIME_RIGHT		480		//time to go SCAN_ANGLE to the right
-#define SPEED_FORWARD	77.28	//[mm/s] for ROT_SPEED = 600 [steps/s]
+#define TIME_RIGHT		480		//time to go (2 x SCAN_ANGLE) to the right
+#define SPEED_FORWARD	77.28	//[mm/s] for AVANCE_SPEED = 600 [steps/s]
 #define TURN_SPEED		2.43	//[rad/s] for ROT_SPEED = 500 [steps/s]
 #define AVOID_MARGIN	2
 
@@ -41,27 +34,28 @@ void avoid_obstacle(uint16_t distance){
 
 	static struct vector vect_left;
 	static struct vector vect_right;
-	static struct vector vect_diff;
+	static struct vector vect_diff;		//vector we use to determine how to avoid the obstacle (angle and distance)
 
 	static uint16_t distance_left;
 	static uint16_t distance_right;
 
 	static float beta;
 	static uint16_t avoid_time;
-	static float straight_time;
+	static uint16_t straight_time;
 
-
+	//robot scans to the left
 	turn_left(TIME_LEFT);
 	distance_left = VL53L0X_get_dist_mm();
 
-	//vecteur du scan a gauche
+	//vector from the scan to the left
 	vect_left.x = -(distance_left * sin(SCAN_ANGLE));
 	vect_left.y = distance_left * cos(SCAN_ANGLE);
 
+	//robot scans to the right
 	turn_right(TIME_RIGHT);
 	distance_right = VL53L0X_get_dist_mm();
 
-	//vecteur du scan a droite
+	//vector from the scan to the right
 	vect_right.x = distance_right * sin(SCAN_ANGLE);
 	vect_right.y = distance_right * cos(SCAN_ANGLE);
 
@@ -94,7 +88,7 @@ void avoid_obstacle(uint16_t distance){
 	}
 
 	else if(distance_left <= distance_right){
-//	else if(phase_diff >= 0){
+
 		vect_diff.x = (-vect_left.x) + vect_right.x;
 		vect_diff.y = (-vect_left.y) + vect_right.y;
 		beta = atan2f(vect_diff.y, vect_diff.x);
@@ -107,7 +101,6 @@ void avoid_obstacle(uint16_t distance){
 	}
 
 	else if(distance_left > distance_right){
-//	else if(phase_diff < 0){
 
 		vect_diff.x = (-vect_left.x) + vect_right.x;
 		vect_diff.y = (-vect_left.y) + vect_right.y;
@@ -185,7 +178,7 @@ uint16_t convert_distance(int16_t distance){
 	return time_forward;
 }
 
-//computes the norm of a vector
+//computes the time the robot needs to go straight to avoid the obstacle
 uint16_t straight_avoid(float component_x, float component_y, float angle, uint16_t distance){
 
 	float magnitude;
@@ -198,8 +191,7 @@ uint16_t straight_avoid(float component_x, float component_y, float angle, uint1
 	perp_dist = distance * sin(gamma);
 
 	magnitude = sqrtf(component_x*component_x + component_y*component_y);
-	chprintf((BaseSequentialStream *)&SD3, "perp_dist = %d\n", perp_dist);
-	chprintf((BaseSequentialStream *)&SD3, "magnitude = %f\n", magnitude);
+
 	dist_to_go = AVOID_MARGIN * magnitude + perp_dist;
 	time_to_go = convert_distance(dist_to_go);
 
